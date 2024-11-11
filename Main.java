5e7ab0a706d097d6085b4731829b52fc384d90b7
SteffanClemente <steffanclemente@gmail.com>
2024-11-11 16:22:19 +1000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,57 @@
+public class Main {
+    public static void main(String[] args) {
+//        for (int j = 0; j < 10; j++) {
+//            for (int i = 0; i > j; i++) {
+//                System.out.print("*");
+//            }
+//            System.out.println(" ");
+//        System.out.println("       *");
+//        System.out.println("      *");
+//        System.out.println("     *");
+//        System.out.println("    *");
+//        System.out.println("   *");
+//        System.out.println("  *");
+//        System.out.println(" *");
+//        System.out.println("*");
+
+//        for (int i = 0; i < 8; i++) {
+//            for (int j = 8; j > i; j--) {
+//                System.out.print(" ");
+
+//            }
+//            System.out.println("*");
+//        }
+        System.out.println("       *       ");
+        System.out.println("      ***      ");
+        System.out.println("     *****     ");
+        System.out.println("    *******    ");
+        System.out.println("   *********   ");
+        System.out.println("  ***********  ");
+        System.out.println(" ************* ");
+        System.out.println("***************");
+
+
+//        for (int i = 0; i < 8; i++) {
+//            for (int j = 0; j < i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+
+
+
+
+
+
+
+
+
+        }
+
+    }
