a3a33c85611d316754138c0e6ffa5eebc26f0dff
SteffanClemente <steffanclemente@gmail.com>
2024-11-13 07:51:04 +1000

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 1; i < 10; i++) {
+            for (int j = 10; j > i; j--) {
+                System.out.print("1");
+            }
+            for (int k = 1; k < i*2; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
